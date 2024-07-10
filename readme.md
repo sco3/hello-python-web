20b9439a4ae74117edd95b556fed588a17da6bbd
sco3 <scorta@gmail.com>
2024-07-10 14:42:53 +0100

    wrk test

--- readme.md
+++ readme.md
@@ -1,18 +1,13 @@
 
+
 uvicorn
 ---
-16 processes 1000 requests
-
-real	0m12.519s
-user	0m56.979s
-sys	1m17.737s
-
-
-fastwsgi
----
-16 processes 1000 requests
-
-real	0m11.497s
-user	1m1.054s
-sys	1m26.871s
+Running 10s test @ http://127.0.0.1:8000
+  2 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency    14.45ms  578.70us  25.00ms   92.45%
+    Req/Sec     6.95k   185.07     7.42k    78.50%
+  138273 requests in 10.00s, 19.91MB read
+Requests/sec:  13821.34
+Transfer/sec:      1.99MB
 
--- test.sh
+++ test.sh
@@ -1,37 +1,7 @@
 #!/usr/bin/env -S bash
 
-#set -ueo pipefail
-
-function run_curl() {
-	#> ${server}.$1.txt
-	local i
-	for ((i = 0; i < $2; i++)); do
-		curl -s localhost:8000 >/dev/null
-		#>>${server}.$1.txt
-	done
-}
-
-function check() {
-	ps -aef | grep $(basename $0) | grep -v grep | wc -l
-}
-
-function main() {
-	echo $1 processes $2 requests
-	for ((i = 0; i < $1; i++)); do
-		run_curl $i $2 &
-	done
-
-	cnt=$(check)
-	while ((cnt > 2)); do
-		cnt=$(check)
-		sleep 1
-	done
-}
-
 server=$(ps -aef | grep -v grep | grep -E -o 'uvicorn|fastwsgi' | sort -u)
 
 echo $server
 echo ---
-time main 16 1000
-
-#wc -l *.txt
+wrk http://127.0.0.1:8000 -d 10 -t 2 -c 200
