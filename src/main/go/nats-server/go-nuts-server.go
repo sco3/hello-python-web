e09cf4f94c7cf892c806885830c0560b32e03d8b
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-07-17 21:02:21 +0100

    go server works

--- src/main/go/nats-server/go-nuts-server.go
+++ src/main/go/nats-server/go-nuts-server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/nats-io/nats.go"
@@ -12,7 +11,20 @@ const hello string = "Hello, world!\n"
 func main() {
 	var hello_bytes []byte = []byte(hello)
 	// Connect to NATS server
-	nc, err := nats.Connect(nats.DefaultURL)
+	opts := nats.Options{
+		Servers: []string{
+			"nats://127.0.0.1:4222", //
+			"nats://127.0.0.1:4223", //
+			"nats://127.0.0.1:4224", //
+		},
+		User:     "sys",
+		Password: "pass",
+	}
+	// nc, err := nats.Connect( //
+	// 	"nats://127.0.0.1:4222,nats://127.0.0.1:4223,nats://127.0.0.1:4224", //
+	// 	Options.User(), // change &opts to opts
+	// )
+	nc, err := opts.Connect()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,7 +32,7 @@ func main() {
 
 	// Subscribe to the "ping" subject
 	nc.Subscribe("req.*", func(m *nats.Msg) {
-		fmt.Printf("Received: %s %s\n", string(m.Data), m.Subject)
+		//fmt.Printf("Received: %s %s\n", string(m.Data), m.Subject)
 		nc.Publish("res."+m.Subject[4:], hello_bytes)
 	})
 
