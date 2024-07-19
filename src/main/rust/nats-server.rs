1300f92221bd37cbd85adb7b37d80502132937d2
sco3 <scorta@gmail.com>
2024-07-19 11:40:34 +0100

    rust nats-server

---
+++ src/main/rust/nats-server.rs
@@ -0,0 +1,30 @@
+//use bytes::Bytes;
+use async_nats::ConnectOptions;
+use futures::StreamExt;
+#[tokio::main]
+async fn main() -> Result<(), async_nats::Error> {
+    println!("start");
+    // set user and password
+    let conn_opts = ConnectOptions::new() //
+        .user_and_password("sys".to_string(), "pass".to_string());
+
+    // Connect to the NATS server
+
+    let client = async_nats::connect_with_options("localhost:4222", conn_opts).await?;
+
+    // Subscribe to the "messages" subject
+    let mut subscriber = client.subscribe("messages").await?;
+
+    // Receive and process messages
+    while let Some(message) = subscriber.next().await {
+        println!("Received message {:?}", message);
+        if let Some(subj) = message.reply {
+            println!("reply subj: {}", subj);
+            client.publish(subj, "Hello, world!\n".into()).await?;
+        }
+    }
+
+    println!("done");
+
+    Ok(())
+}
