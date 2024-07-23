9b90fdb42daab047241cd7357c951e8bbfa65e6a
sco3 <scorta@gmail.com>
2024-07-23 18:56:20 +0100

    reactive client threads pool size 100

--- src/main/java/sco3/NatsSquareClientReactor.java
+++ src/main/java/sco3/NatsSquareClientReactor.java
@@ -1,16 +1,20 @@
 package sco3;
 
+import static java.lang.Thread.sleep;
+import static java.lang.ThreadLocal.withInitial;
+import static java.util.concurrent.Executors.newFixedThreadPool;
 import static reactor.core.publisher.Mono.fromFuture;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import io.nats.client.Connection;
 import io.nats.client.Message;
 import io.nats.client.Nats;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 public class NatsSquareClientReactor implements NatsSquare {
@@ -33,11 +37,12 @@ public class NatsSquareClientReactor implements NatsSquare {
 		return toInt(m.getData());
 	}
 
-	public long run(Connection nc) throws Exception {
+	public long call(ThreadLocal<Connection> tnc) throws Exception {
 		long start = System.currentTimeMillis();
 		int n = 1000;
 		ArrayList<Integer> result = new ArrayList<>(n);
 		final AtomicBoolean run = new AtomicBoolean(true);
+		final Connection nc = tnc.get();
 
 		Flux<Integer> flux = Flux.range(1, n) //
 				.map(i -> toBytes(i)) //
@@ -49,26 +54,62 @@ public class NatsSquareClientReactor implements NatsSquare {
 		while (run.get()) {
 			Thread.sleep(1);
 		}
+		// System.out.println(result);
 
 		long finish = System.currentTimeMillis();
 		return finish - start;
 	}
 
-	private void runManyTimes(Connection nc) throws IOException, InterruptedException, Exception {
-		long dur = 0;
+	private void runManyTimes() throws Exception {
+		long start = System.currentTimeMillis();
+
+		int poolSize = 100;
+		ConcurrentLinkedQueue<Connection> conns = new ConcurrentLinkedQueue<>();
+
+		ThreadPoolExecutor svc = (ThreadPoolExecutor) newFixedThreadPool(poolSize);
+		ThreadLocal<Connection> nc = withInitial(//
+				() -> {
+					try {
+						Connection c = Nats.connect("nats://localhost:4222");
+						conns.add(c);
+						return c;
+					} catch (IOException | InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						return null;
+					}
+				}//
+		);
+
 		int tests = 1000;
 		for (int i = 0; i < tests; i++) {
-			dur += run(nc);
+			svc.execute(new Runnable() {
+				@Override
+				public void run() {
+					long l = 0;
+					try {
+						l = call(nc);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
 		}
-		nc.close();
-		System.out.println("Time -> " + dur + " ms avg: " + ((double) dur) / tests);
+		while (svc.getCompletedTaskCount() < tests) {
+			sleep(1);
+		}
+		long finish = System.currentTimeMillis();
+		long duration = finish - start;
+		System.out.println("Time -> " + duration + " ms avg: " + ((double) duration) / tests);
+		svc.shutdown();
+		for (Connection c : conns) {
+			c.close();
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		Connection nc = Nats.connect("nats://localhost:4222");
 		NatsSquareClientReactor client = new NatsSquareClientReactor();
-		client.runManyTimes(nc);
-		nc.close();
+		client.runManyTimes();
 	}
 
 }
