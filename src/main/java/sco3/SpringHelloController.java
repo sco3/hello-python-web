d389d7fa04900d619fedcb6447a172fd51ea9f4d
sco3 <scorta@gmail.com>
2024-07-24 12:06:52 +0100

    spring boot

--- src/main/java/sco3/SpringHelloController.java
+++ src/main/java/sco3/SpringHelloController.java
@@ -3,12 +3,14 @@ package sco3;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
 @RestController
 public class SpringHelloController {
 
 	@GetMapping("/")
-	public String getHello() {
-		return "Hello, World!\n";
+	public Mono<String> getHello() {
+		return Mono.just("Hello, World!\n");
 	}
 
 }
