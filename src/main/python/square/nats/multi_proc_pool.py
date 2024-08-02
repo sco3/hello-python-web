f2570b8d8b35f0d21cac20d6d93a827b02010155
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-08-02 02:08:56 +0100

    multiproc

--- src/main/python/square/nats/multi_proc_pool.py
+++ src/main/python/square/nats/multi_proc_pool.py
@@ -1,35 +1,51 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 import os
 import time
-
-res = []
-
-
-def hello_world(name):
-    print("enter:", os.getpid())
-    time.sleep(1)
-    print("exit:", os.getpid())
-    return name.upper()
+import asyncio
+from nats_reactor import NatsReactor
 
 
-def collect(name):
-    res.append(name)
+class MultiProcessor:
+    def __init__(self, num_processes=None):
+        self.num_processes = num_processes or os.cpu_count()
+        self.results = []
+        self.manager = Manager()
+        self.manager.Value ()
 
+    async def execute(self, name):
+        reactor = self.manager.Value ("reactor")
+        if not reactor:
+            reactor = NatsReactor()
+            reactor.connect_nats()
+            self.storage.reactor = reactor
 
-def demo_multi_processing():
-    tic = time.time()
-    pool = Pool(processes=os.cpu_count())
+        reactor = self.storage.reactor
+        return reactor.aggregate(1000)
 
-    for i in range(10):
-        pool.apply_async(hello_world, args=(f"x{i}",), callback=collect)
+    def wrapper(self, name):
+        print("enter:", os.getpid())
+        loop = asyncio.get_event_loop()
+        r = loop.run_until_complete(self.execute(name))
+        print(r)
+        return r
 
-    pool.close()
-    pool.join()
+    def collect(self, result):
+        self.results.append(result)
 
-    print(res)
-    toc = time.time()
-    print(f"Completed in {toc - tic} seconds")
+    def run(self):
+        tic = time.time()
+        with Pool(processes=self.num_processes) as pool:
+            for i in range(10):
+                pool.apply_async(self.wrapper, args=(f"x{i}",), callback=self.collect)
+
+            pool.close()
+            pool.join()
+
+        print(self.results)
+        toc = time.time()
+        print(f"Completed in {toc - tic} seconds")
 
 
 if __name__ == "__main__":
-    demo_multi_processing()
+    processor = MultiProcessor()
+    processor.run()
