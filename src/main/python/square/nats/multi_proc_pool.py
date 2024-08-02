ca5bbebd924293a918b0a07876e9daaf6505dc4b
sco3 <scorta@gmail.com>
2024-08-02 10:13:21 +0100

    works but not in class

--- src/main/python/square/nats/multi_proc_pool.py
+++ src/main/python/square/nats/multi_proc_pool.py
@@ -1,51 +1,32 @@
-from multiprocessing import Pool, Manager
-import os
+import multiprocessing as mp
 import time
-import asyncio
-from nats_reactor import NatsReactor
 
 
-class MultiProcessor:
-    def __init__(self, num_processes=None):
-        self.num_processes = num_processes or os.cpu_count()
-        self.results = []
-        self.manager = Manager()
-        self.manager.Value ()
+async def a_call():
+    return x * x
 
-    async def execute(self, name):
-        reactor = self.manager.Value ("reactor")
-        if not reactor:
-            reactor = NatsReactor()
-            reactor.connect_nats()
-            self.storage.reactor = reactor
 
-        reactor = self.storage.reactor
-        return reactor.aggregate(1000)
+def foo_pool(x):
+    return x * x
 
-    def wrapper(self, name):
-        print("enter:", os.getpid())
-        loop = asyncio.get_event_loop()
-        r = loop.run_until_complete(self.execute(name))
-        print(r)
-        return r
 
-    def collect(self, result):
-        self.results.append(result)
+result_list = []
 
-    def run(self):
-        tic = time.time()
-        with Pool(processes=self.num_processes) as pool:
-            for i in range(10):
-                pool.apply_async(self.wrapper, args=(f"x{i}",), callback=self.collect)
 
-            pool.close()
-            pool.join()
+def log_result(result):
+    # This is called whenever foo_pool(i) returns a result.
+    # result_list is modified only by the main process, not the pool workers.
+    result_list.append(result)
 
-        print(self.results)
-        toc = time.time()
-        print(f"Completed in {toc - tic} seconds")
+
+def apply_async_with_callback():
+    pool = mp.Pool()
+    for i in range(10):
+        pool.apply_async(foo_pool, args=(i,), callback=log_result)
+    pool.close()
+    pool.join()
+    print(result_list)
 
 
 if __name__ == "__main__":
-    processor = MultiProcessor()
-    processor.run()
+    apply_async_with_callback()
