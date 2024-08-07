a928c1e2920c1643d97ca72ffe3e62a8cc94b7ba
sco3 <scorta@gmail.com>
2024-08-07 15:30:52 +0100

    go batch client 42 seconds

--- src/main/go/nats-reply-square-client/go-nats-reply-square-client.go
+++ src/main/go/nats-reply-square-client/go-nats-reply-square-client.go
@@ -3,16 +3,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
 	"strconv"
 
 	"github.com/nats-io/nats.go"
 )
 
-func main() {
+func call(nc *nats.Conn, num int) int {
+	var subj string = "square"
 
-	opts := nats.Options{
+	var sNum string = strconv.Itoa(num)
+	var res *nats.Msg
+	res, _ = nc.Request(subj, []byte(sNum), time.Hour)
+	sNum = string(res.Data)
+	var outNum int
+	outNum, _ = strconv.Atoi(sNum)
+	return outNum
+}
+
+func aggregate(num int, nc *nats.Conn) []int {
+	var result = make([]int, num)
+	var i int
+	for i = 0; i < num; i++ {
+		var outNum = call(nc, i+1)
+		result[i] = outNum
+	}
+	return result
+}
+
+func main() {
+	const nTests int = 1000
+	const number int = 1000
+	var start time.Time
+	start = time.Now()
+
+	var opts = nats.Options{
 		Servers: []string{
 			"nats://127.0.0.1:4222", //
 			"nats://127.0.0.1:4223", //
@@ -21,24 +49,21 @@ func main() {
 		User:     "sys",
 		Password: "pass",
 	}
-	nc, err := opts.Connect()
+	var nc, err = opts.Connect()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer nc.Close()
 
-	subj := "square"
+	var results map[int]struct{} = make(map[int]struct{})
+	for i := 0; i < nTests; i++ {
+		result := aggregate(number, nc)
+		results[len(result)] = struct{}{}
+	}
 
-	nc.QueueSubscribe(subj, "worker", func(m *nats.Msg) {
-		i, _ := strconv.Atoi(string(m.Data))
-		i = i * i
-		hello_bytes := []byte(strconv.Itoa(i))
-		err = m.Respond(hello_bytes)
-		if err != nil {
-			log.Printf("Failed to respond to message: %v", err)
-		}
-	})
-	println("listening:", subj)
-	// Keep the connection alive
-	select {}
+	var dur time.Duration
+	dur = time.Now().Sub(start)
+
+	fmt.Printf("Took: %#0.4f ms %v", 1000*dur.Seconds(), results)
+
 }
