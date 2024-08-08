f519ab1a8a1f992e3e6110ecf7f273ee5f7c5254
sco3 <scorta@gmail.com>
2024-08-08 18:34:34 +0100

    works without channel result is in correct order

--- src/main/go/nats-reply-square-client-goroutines/go-nats-reply-square-client-goroutines.go
+++ src/main/go/nats-reply-square-client-goroutines/go-nats-reply-square-client-goroutines.go
@@ -13,10 +13,7 @@ import (
 	"github.com/nats-io/nats.go"
 )
 
-func call( //
-	nc *nats.Conn, num int, results chan int, //
-	wg *sync.WaitGroup, mu *sync.Map, //
-) {
+func call(nc *nats.Conn, num int, results chan int, wg *sync.WaitGroup, mu *sync.Mutex, i2 int, i *[]int) {
 	defer wg.Done()
 	const subj string = "square"
 	var sNum string = strconv.Itoa(num)
@@ -25,26 +22,32 @@ func call( //
 	sNum = string(res.Data)
 	var outNum int
 	outNum, _ = strconv.Atoi(sNum)
+	mu.Lock()
+	(*i)[i2] = outNum
+	mu.Unlock()
 	//results <- outNum
-	mu.Store(num, outNum)
+	//mu.Store(num, outNum)
+
 }
 
 func aggregate(num int, nc *nats.Conn) []int {
 	var wg sync.WaitGroup
 	wg.Add(num)
 	var resChannels chan int = make(chan int, num)
-	var mu sync.Map
+	var mu sync.Mutex
+	var a []int = make([]int, num)
 	for i := 0; i < num; i++ {
-		go call(nc, i+1, resChannels, &wg, &mu)
+		go call(nc, i+1, resChannels, &wg, &mu, i, &a)
 	}
 	wg.Wait()
 	close(resChannels)
-	var a []int = make([]int, num)
-	for i := 0; i < num; i++ {
-		if value, ok := mu.Load(i); ok {
-			a[i] = value.(int)
+	/*
+		for i := 0; i < num; i++ {
+			if value, ok := mu.Load(i); ok {
+				a[i] = value.(int)
+			}
 		}
-	}
+	*/
 	/*
 		var i int = 0
 		for r := range resChannels {
@@ -53,14 +56,14 @@ func aggregate(num int, nc *nats.Conn) []int {
 		}
 	*/
 	//sort.Ints(a)
-	fmt.Printf("%v\n", a)
+	//fmt.Printf("%v\n", a)
 	return a
 }
 
 func main() {
 	runtime.GOMAXPROCS(1)
-	const nTests int = 10
-	const number int = 10
+	const nTests int = 1000
+	const number int = 1000
 	var start time.Time
 	start = time.Now()
 
