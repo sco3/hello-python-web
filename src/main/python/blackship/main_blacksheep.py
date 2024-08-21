149dbb2522e732172583f316acc67d4756a2a47a
sco3 <scorta@gmail.com>
2024-08-21 16:19:41 +0100

    blacksheep

---
+++ src/main/python/blackship/main_blacksheep.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S poetry run python
+
+import uvicorn
+import subprocess
+from datetime import datetime
+
+from blacksheep import Application, get
+
+
+app = Application()
+
+
+@get("/")
+async def home():
+    return f"Hello, World!\n"
+
+
+if __name__ == "__main__":
+
+    command = [
+        "uvicorn",
+        "main_blacksheep:app",
+        "--port",
+        "8000",
+        "--host",
+        "0.0.0.0",
+        "--log-level",
+        "critical",
+    ]
+    subprocess.run(command)
