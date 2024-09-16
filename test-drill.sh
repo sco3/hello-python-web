4285b0f005a996178d9ab982e94340068a4978fe
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-09-17 00:38:20 +0100

    drill

---
+++ test-drill.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash
+
+
+# cargo install drill
+
+drill -q --benchmark test-drill.yaml --stats
--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ echo $server
 echo ---
 echo ''
 echo '```'
-wrk http://127.0.0.1:8000 -d 10 -t 2 -c 100 
+wrk http://127.0.0.1:8000 -d 10 -t 300 -c 300
 #-H "Hello, world\n"
 echo '```'
