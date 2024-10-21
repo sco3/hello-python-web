829d7249f8da077c74f70e8e6acdb9978a4e81a1
sco3 <scorta@gmail.com>
2024-10-21 17:36:42 +0100

    starlette still slower than blackship

--- src/main/python/uvc-starlette/uvc_starlette.py
+++ src/main/python/uvc-starlette/uvc_starlette.py
@@ -7,13 +7,13 @@ from starlette.applications import Starlette
 from starlette.responses import PlainTextResponse
 from starlette.routing import Route
 
+
 async def home(request):
     return PlainTextResponse("Hello, World!")
 
-app = Starlette(routes=[
-    Route("/", home)
-])
+
+app = Starlette(routes=[Route("/", home)])
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1, log_level="warning")
