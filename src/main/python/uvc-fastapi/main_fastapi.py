e4607d31c89a8c9155db25f51ed56c96653f02a6
sco3 <scorta@gmail.com>
2024-12-04 13:39:06 +0000

    ws and deps

--- src/main/python/uvc-fastapi/main_fastapi.py
+++ src/main/python/uvc-fastapi/main_fastapi.py
@@ -49,9 +49,12 @@ async def home(reso=Depends(MyClass.resource)):
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
 
 
 if __name__ == "__main__":
