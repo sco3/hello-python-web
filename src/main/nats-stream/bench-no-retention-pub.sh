81ba5f770a923f983af586d0aaf5f62cec5b97b1
sco3 <scorta@gmail.com>
2025-01-03 20:54:00 +0000

    test

--- src/main/nats-stream/bench-no-retention-pub.sh
+++ src/main/nats-stream/bench-no-retention-pub.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env -S bash
 
-set -xueo pipefail
 
-SIZE=${1:-1048664}
-
-# limit total size to ~ 8g
-TOTAL=8000000000
-NUM=$((90*$TOTAL/$SIZE/100))
-
+source 00-sizes.sh
 
 nats bench test --no-progress --js --pub 1  --size $SIZE --msgs 5 --maxbytes 8G   2>&1 | tee $0.out.$SIZE
