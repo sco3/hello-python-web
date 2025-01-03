383887e2753d36f032db5a289fdf6c895e2e4d44
sco3 <scorta@gmail.com>
2025-01-03 14:06:53 +0000

    test

--- src/main/kafka/test-sub.sh
+++ src/main/kafka/test-sub.sh
@@ -1,2 +1,19 @@
-~/prg/kafka/bin/kafka-consumer-perf-test.sh --bootstrap-server localhost:33001  --topic asdf --messages 10000000 
+#!/usr/bin/env -S bash
+
+set -xueo pipefail
+
+SIZE=${1:-1048664}
+
+# limit total size to ~ 7g
+TOTAL=8000000000
+NUM=$(($TOTAL/$SIZE))
+OUT=$0.out.$SIZE
+
+
+~/prg/kafka/bin/kafka-consumer-perf-test.sh \
+--bootstrap-server localhost:33001  \
+--topic asdf --messages $NUM  | tee $OUT
+
+
+awk -F',' '{ print $6}' $OUT > $OUT.msg.per.sec
 
