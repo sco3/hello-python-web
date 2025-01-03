51b4dc4638f21cccf6ee39ef0ad2f0a1ea31f04d
sco3 <scorta@gmail.com>
2025-01-03 10:54:27 +0000

    kafka start

--- src/main/kafka/start.sh
+++ src/main/kafka/start.sh
@@ -1,6 +1,10 @@
 
 
-#~/prg/confluent/bin/kafka-storage format --config cluster.cfg --cluster-id asdf
+
+sed  "s!log.dirs=.+$!log.dirs=$HOME/kafka-logs" cluster.cfg
 
 
- ~/prg/confluent/bin/kafka-server-start cluster.cfg
+~/prg/confluent/bin/kafka-storage format --config cluster.cfg --cluster-id asdf
+
+
+#~/prg/confluent/bin/kafka-server-start cluster.cfg
--- src/main/python/uvc-fastapi/main_background_task.py
+++ src/main/python/uvc-fastapi/main_background_task.py
@@ -6,7 +6,7 @@ import gc
 import threading
 import uuid
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 import uvicorn
 
 
@@ -15,10 +15,18 @@ app = FastAPI()
 state_var = contextvars.ContextVar("state")
 tasks = set()
 
+class State:
+    def __init__ (self):
+        self.state = ["?"]
+
+    def __str__ (self):
+        return str(self.state)
+
 
 async def simple_task():
     await asyncio.sleep(10)
-    print("Simple task", state_var.get())
+    state_var.get().state.append(uuid.uuid4())
+    print("Simple task", str(state_var.get()))
 
 
 def count_context_objects():
@@ -40,9 +48,16 @@ def completion(task: asyncio.Task):
     tasks.discard(task)
 
 
+@app.middleware("http")
+async def mw(request: Request, call_next):
+    state = State()
+    state.state.append(uuid.uuid4())
+    state_var.set(state)
+    return await call_next(request)
+
+
 @app.get("/start-task")
 async def start_task():
-    state_var.set(uuid.uuid4())
     background_task = asyncio.create_task(simple_task())
     background_task.add_done_callback(completion)
     tasks.add(background_task)
@@ -50,6 +65,8 @@ async def start_task():
 
 
 if __name__ == "__main__":
+    state = State()
+    print (state)
     threading.Thread(target=count_context_objects, daemon=True).start()
     uvicorn.run(
         "main_background_task:app",
