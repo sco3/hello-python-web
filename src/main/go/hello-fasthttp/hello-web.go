d09e3cb1b30eb046bd1c0494613d9d539c7ace1c
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-08-07 17:43:49 +0100

    test

---
+++ src/main/go/hello-fasthttp/hello-web.go
@@ -0,0 +1,21 @@
+///usr/bin/true; exec /usr/bin/env go run "$0" "$@"
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+			fmt.Fprint(ctx, "Hello, world!\n")
+	}
+
+	if err := fasthttp.ListenAndServe(":8000", requestHandler); err != nil {
+		fmt.Printf("Error in ListenAndServe: %s\n", err)
+	}
+}
--- src/main/python/granian/main_fastapi.sh
+++ src/main/python/granian/main_fastapi.sh
@@ -1,1 +1,1 @@
-poetry run granian --interface asgi main_fastapi:app
+uv run granian --interface asgi main_fastapi:app
